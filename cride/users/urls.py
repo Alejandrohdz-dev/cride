faaf6984236826270436cb843d31009c11cfd4d9
Alejandro Hernandez <ahernandez@linets.cl>
2021-12-21 19:44:23 -0400

    [ADD] circles services

--- cride/users/urls.py
+++ cride/users/urls.py
@@ -8,5 +8,4 @@ urlpatterns = [
     path('users/login/', UserLoginAPIView.as_view(), name='login'),
     path('users/signup/', UserSignupAPIView.as_view(), name='signup'),
     path('users/verify/', AccountVerificationAPIView.as_view(), name='verify'),
-
 ]
