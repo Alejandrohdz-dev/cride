85ce0b3375e51da173cc5368a2c4d770cbdfa392
Alejandro Hernandez <ahernandez@linets.cl>
2021-12-21 19:44:03 -0400

    [UPDATE] name of a field members_limit

--- cride/circles/admin.py
+++ cride/circles/admin.py
@@ -8,6 +8,6 @@ from cride.circles.models import Circle
 class CircleAdmin(admin.ModelAdmin):
     """Circle admin."""
 
-    list_display = ('slug_name', 'name', 'is_public', 'verified', 'is_limited', 'members_limite')
+    list_display = ('slug_name', 'name', 'is_public', 'verified', 'is_limited', 'members_limit')
     search_fields = ('slug_name', 'name')
     list_filter = ('is_public', 'verified', 'is_limited')
--- cride/circles/models/circles.py
+++ cride/circles/models/circles.py
@@ -28,7 +28,7 @@ class Circle(CRideModel):
     is_public = models.BooleanField(default=True, help_text='Public circles are listed in the main page so every know about their existence')
 
     is_limited = models.BooleanField('limited', default=False, help_text='Limited circles can grow up to a fixed number of members')
-    members_limite = models.PositiveIntegerField(default=0, help_text='If circle is limited, this will be the limit on the number of members')
+    members_limit = models.PositiveIntegerField(default=0, help_text='If circle is limited, this will be the limit on the number of members')
 
     def __str__(self):
         """Return circle name"""
