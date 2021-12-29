c8bb89200d0d1da78b445954db1bd567227b9464
Alejandro Hernandez <ahernandez@linets.cl>
2021-12-29 07:48:57 -0400

    [ADD] circles route to urls

--- cride/circles/urls.py
+++ cride/circles/urls.py
@@ -6,12 +6,18 @@ from django.urls import path, include
 # Django REST Framework
 from rest_framework.routers import DefaultRouter
 
-# model
+# Local
 from .views import circles as circles_views
+from .views import memberships as membership_views
 
 router = DefaultRouter()
 
 router.register(r'circles', circles_views.CircleViewSet, basename='circle')
+router.register(
+    r'circles/(?P<slug_name>[-a-zA-Z0-0_]+)/members',
+    membership_views.MembershipViewSet,
+    basename='membership'
+)
 
 urlpatterns = [
     path('', include(router.urls))
