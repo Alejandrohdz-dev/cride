753c3b7ab120e7804511ffc3eefa71182c0474a3
Alejandro Hernandez <ahernandez@linets.cl>
2021-12-30 11:15:44 -0400

    [ADD] IsSelfMember permission

--- cride/circles/permissions/memberships.py
+++ cride/circles/permissions/memberships.py
@@ -23,3 +23,16 @@ class IsActiveCircleMember(BasePermission):
         except Memberships.DoesNotExist:
             return False
         return True
+
+
+class IsSelfMember(BasePermission):
+    """Allow access only to member owners."""
+
+    def has_permission(self, request, view):
+        """Let object permission grant access."""
+        obj = view.get_object()
+        return self.has_object_permission(request, view, obj)
+
+    def has_object_permission(self, request, view, obj):
+        """Allow access only if member is owned by the requesting user."""
+        return request.user == obj.user
