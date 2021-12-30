d18c96bc36014f768ae041165de36339801ebd44
Alejandro Hernandez <ahernandez@linets.cl>
2021-12-30 11:14:36 -0400

    [UPDATE] Membership viewset

--- cride/circles/views/memberships.py
+++ cride/circles/views/memberships.py
@@ -1,17 +1,23 @@
 """Circle memberships views."""
 
 # Django Rest Framework
-from rest_framework.permissions import IsAuthenticated
 from rest_framework import mixins, viewsets
+from rest_framework import response
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.generics import get_object_or_404
+from rest_framework import status
 
 # Local
-from cride.circles.permissions.memberships import IsActiveCircleMember
-from cride.circles.serializers.memberships import MembershipsModelSerializer
+from cride.circles.permissions.memberships import IsActiveCircleMember, IsSelfMember
+from cride.circles.serializers.memberships import MembershipsModelSerializer, AddMemberSerializer
 from cride.circles.models.circles import Circle
 from cride.circles.models.memberships import Memberships
+from cride.circles.models.invitations import Invitation
 
 class MembershipViewSet(mixins.ListModelMixin,
+                        mixins.CreateModelMixin,
                         mixins.RetrieveModelMixin,
                         mixins.DestroyModelMixin,
                         viewsets.GenericViewSet):
@@ -26,7 +32,11 @@ class MembershipViewSet(mixins.ListModelMixin,
 
     def get_permissions(self):
         """Assign permissions based on action."""
-        permissions = [IsAuthenticated, IsActiveCircleMember]
+        permissions = [IsAuthenticated]
+        if self.action != 'create':
+            permissions.append(IsActiveCircleMember)
+        if self.action == 'invitations':
+            permissions.append(IsSelfMember)
         return [p() for p in permissions]
 
     def get_queryset(self):
@@ -49,3 +59,52 @@ class MembershipViewSet(mixins.ListModelMixin,
         """Disable membership."""
         instance.is_active = False
         instance.save()
+
+    @action(detail=True, methods=['GET'])
+    def invitations(self, request, *args, **kwargs):
+        """Retrieve a member's invitations breakdown.
+
+        Will return a list containing all the members that have
+        used its invitations and another list containing the
+        invitations that haven't being used yet.
+        """
+        member = self.get_object()
+        invited_members = Memberships.objects.filter(
+            circle=self.circle,
+            invited_by=request.user,
+            is_active=True
+        )
+
+        unused_invitations = Invitation.objects.filter(
+            circle=self.circle,
+            issued_by=request.user,
+            used=False
+        ).values_list('code')
+
+        diff = member.remaining_invitations - len(unused_invitations)
+
+        invitations = [x[0] for x in unused_invitations]
+        for i in range(0, diff):
+            invitations.append(
+                Invitation.objects.create(
+                    issued_by=request.user,
+                    circle=self.circle
+                ).code
+            )
+
+        data = {
+            'used_invitations': MembershipsModelSerializer(invited_members, many=True),
+            'invitations': invitations
+        }
+        return Response(data)
+
+    def create(self, request, *args, **kwargs):
+        """Handle member creation from invitation code."""
+        serializer = AddMemberSerializer(
+            data=request.data,
+            context={'circle': self.circle, 'request': request}
+        )
+        serializer.is_valid(raise_exceptrion=True)
+        member = serializer.save()
+        data = self.get_serializer(member).data
+        return Response(data, status=status.HTTP_201_CREATED)
